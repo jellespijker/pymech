52bc5a987460f7f3ad3f238feefe9589348912c1
Jelle Spijker <spijker.jelle@gmail.com>
2017-01-29 21:10:26 +0100

    Axle plot function added for fmt

--- pymech/axle/Axle.py
+++ pymech/axle/Axle.py
@@ -1,7 +1,7 @@
 import math as math
-
 import numpy as np
 from IPython.display import Latex
+import matplotlib.pyplot as pl
 
 from pymech.fmt.Solver import Solver
 from pymech.units.SI import ureg
@@ -27,6 +27,33 @@ class Axle:
         self.V = S.V
         self.M = S.M
         self.T = S.T
+
+    def plotfmt(self, figsize=(10,5)):
+        pl.figure(figsize=figsize)
+        pl.subplot(311)
+        pl.plot(self.properties.geometry.axle, self.V)
+        pl.grid(True)
+        pl.title('Vertical shear')
+        pl.ylabel('Force in [N]')
+        pl.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+
+        pl.subplot(312)
+        pl.plot(self.properties.geometry.axle, self.M)
+        pl.grid(True)
+        pl.title('Bending moments')
+        pl.ylabel('Moment in [Nm]')
+        pl.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+
+        pl.subplot(313)
+        pl.plot(self.properties.geometry.axle, self.T)
+        pl.grid(True)
+        pl.title('Torque')
+        pl.ylabel('Torque in [Nm]')
+        pl.xlabel('Length in [mm]')
+        pl.ticklabel_format(style='sci', axis='y', scilimits=(0, 0))
+
+        pl.tight_layout()
+        pl.show()
 
     def d_prime(self, pretty=False):
         if not self.properties.geometry.solved:

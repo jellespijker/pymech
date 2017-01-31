b5c9c914c7bd61ced1b616f052c687b3060c35d2
Jelle Spijker <spijker.jelle@gmail.com>
2017-01-31 20:39:22 +0100

    d_a function added

--- pymech/axle/Axle.py
+++ pymech/axle/Axle.py
@@ -1,11 +1,13 @@
 import math as math
-import numpy as np
-import matplotlib.pyplot as pl
 
+import matplotlib.pyplot as pl
+import numpy as np
+
+import pymech.print.Latex as Latex
 from pymech.fmt.Solver import Solver
 from pymech.units.SI import ureg
-import pymech.print.Latex as Latex
 from .Properties import Properties
+
 
 class Axle:
     properties: Properties
@@ -27,7 +29,7 @@ class Axle:
         self.M = S.M
         self.T = S.T
 
-    def plotfmt(self, figsize=(10,5)):
+    def plotfmt(self, figsize=(10, 5)):
         pl.figure(figsize=figsize)
         pl.subplot(311)
         pl.plot(self.properties.geometry.axle, self.V)
@@ -54,18 +56,55 @@ class Axle:
         pl.tight_layout()
         pl.show()
 
+    def M_max(self, pretty=False):
+        M_max = np.max(self.M) * ureg.N * ureg.m ** -2
+        if pretty:
+            pr = Latex.display(Latex.toStr(r"M_{max} = \max(M_{nom}) \rightarrow " + Latex.toStr(M_max)))
+            return [M_max, pr]
+        else:
+            return M_max
+
+    def M_b(self, pretty=False):
+        M_b = self.M_max() * self.properties.appliancefactor.K_a
+        if pretty:
+            pr = Latex.display(Latex.toStr(r"M_{b} = K_A \max(M_{nom}) \times \rightarrow " + Latex.toStr(
+                self.properties.appliancefactor.K_a) + r" \times " + Latex.toStr(self.M_max()) + r" = " + Latex.toStr(
+                self.M_b())))
+            return [M_b, pr]
+        else:
+            return M_b
+
     def d_prime(self, pretty=False):
         if not self.properties.geometry.solved:
             self.solvegeometry()
-        M_max = np.max(self.M) * self.properties.appliancefactor.K_a * ureg.N * ureg.m**-2
-        M_b = M_max * self.properties.appliancefactor.K_a
-        t = M_b / self.properties.material.sigma_bWN
-        d_prime = 3.4 * math.pow(t, 1 / 3) * ureg.meter
+        d_prime = 3.4 * math.pow(self.M_b() / self.properties.material.sigma_bWN, 1 / 3) * ureg.meter
 
         if pretty:
-            formArray = [[r"M_{b} = K_A \max(M_{nom}) \times \rightarrow " + Latex.toStr(self.properties.appliancefactor.K_a) + r" \times " + Latex.toStr(M_max) + r" = " + Latex.toStr(M_b)],
-                         [r"d^{'} = \approx 3.4 " + Latex.sqrt(Latex.frac(r"M_{b}", r"\sigma_{bWN}"), 3) + r" \rightarrow 3.4 [m] " + Latex.sqrt(Latex.frac(M_b, self.properties.material.sigma_bWN), 3) + r" \approx " + Latex.toStr(d_prime)]]
-            pretty = Latex.display(Latex.array(formArray))
-            return [d_prime, pretty]
+            formArray = [[self.M_b(pretty=True)[1].data[1:-1]],
+                         [r"d^{'} = \approx 3.4 " + Latex.sqrt(Latex.frac(r"M_{b}", r"\sigma_{bWN}"),
+                                                               3) + r" \rightarrow 3.4 [m] " + Latex.sqrt(
+                             Latex.frac(self.M_b(), self.properties.material.sigma_bWN),
+                             3) + r" \approx " + Latex.toStr(d_prime)]]
+            pr = Latex.display(Latex.array(formArray))
+            return [d_prime, pr]
         else:
-            return [d_prime]
+            return d_prime
+
+    def d_a(self, k: float = 0.5, pretty=False):
+        if not self.properties.geometry.solved:
+            self.solvegeometry()
+        d_a = 3.4 * math.pow(self.M_b() / ((1 - math.pow(k, 4)) * self.properties.material.sigma_bWN),
+                             1 / 3) * ureg.meter
+
+        if pretty:
+            formArray = [[self.M_b(pretty=True)[1].data[1:-1]],
+                         [r"d_{a} = \approx 3.4 " + Latex.sqrt(
+                             Latex.frac(r"M_{b}", r"\left(1 - k^4 \right) \sigma_{bWN}"),
+                             3) + r" \rightarrow 3.4 [m] " + Latex.sqrt(Latex.frac(self.M_b(),
+                                                                                   r"\left(1 - " + str(k) + r"^{4} " + r"\right)" + Latex.toStr(
+                                                                                       self.properties.material.sigma_bWN)),
+                                                                        3) + r" \approx " + Latex.toStr(d_a)]]
+            pr = Latex.display(Latex.array(formArray))
+            return [d_a, pr]
+        else:
+            return d_a
